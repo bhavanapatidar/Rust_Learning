c027f31c509c6e698aa1a0122cac49bc62015509
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-04 00:18:24 +0530

    Clone Method

--- LearnRust/exercise1/avoiding_ownership/clone_method.rs
+++ LearnRust/exercise1/avoiding_ownership/clone_method.rs
@@ -1,10 +1,11 @@
 // before understanding this section once go through ownership part in that retaking ownership i have discussed 
 //we can avoid ownership using clone method and retaking ownership using tuple
-// clone method is use for the deep copy of the heap data. This is an expensive method.
+// clone method is use for the deep copy of the heap data. This is an expensive method .
 
 
 fn main(){
 let s1 = String::from("hello");
 let s2 = s1.clone();
 println!("s1 = {}, s2 = {}", s1,s2);
+
 }
