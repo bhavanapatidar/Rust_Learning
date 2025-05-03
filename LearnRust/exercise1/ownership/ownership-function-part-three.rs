97fe84326428035ca73ef377eaf3ee9a78f7161c
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-03 22:57:01 +0530

    A perfect example

---
+++ LearnRust/exercise1/ownership/ownership-function-part-three.rs
@@ -0,0 +1,16 @@
+fn main(){
+    let s1:String = get_string();
+    println!("this is s1:{}",s1);
+
+    let s2:String = String::from("World");
+    let s3:String = send_get_string(s2);
+
+    println!("this is s3:{}",s3);
+}
+fn get_string() -> String{
+    let new_string = String::from("hello");
+    return new_string;
+}
+fn send_get_string(received_string:String)->String{
+    return received_string;
+}
