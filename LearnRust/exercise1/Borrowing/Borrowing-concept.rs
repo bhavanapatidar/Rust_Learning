cf4f0ea9ce6925749605564452d0104b5ce78f99
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-04 13:34:34 +0530

    Borrowing concept with explaination

---
+++ LearnRust/exercise1/Borrowing/Borrowing-concept.rs
@@ -0,0 +1,16 @@
+// the function borrows 's1' without taking ownership.it can read the content of 's1' but it can not change or modify it 
+// when you want to pass data to a function or share it between parts of your program, you can choose to lend or (borrow ) ownership of the data rather than transferring ownership outright.
+//Borrowing allows you to temporarily use a reference to the data without taking ownership of it .
+
+
+fn  main(){
+    let s1:String = String::from("Hello");
+    let len:usize = calculate_length(&s1); //borrow operation
+    println!("the length of {} is {}", s1,len);
+}
+fn calculate_length(s2:&String)->usize{
+    return s2.len();
+}
+
+//the &s1 syntax lets us create a reference that refers to the value of s1 but does not own it.
+//Because 's' does not own 's1' when 's' goes out of scope nothing happens
