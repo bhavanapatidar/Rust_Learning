5b84d562a3842b29eee4d356d77d917677e8f83b
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-04 14:32:47 +0530

    Borrowing concept with explaination

--- LearnRust/exercise1/Borrowing/Borrowing-concept.rs
+++ LearnRust/exercise1/Borrowing/Borrowing-concept.rs
@@ -14,3 +14,15 @@ fn calculate_length(s2:&String)->usize{
 
 //the &s1 syntax lets us create a reference that refers to the value of s1 but does not own it.
 //Because 's' does not own 's1' when 's' goes out of scope nothing happens
+
+// fn main(){
+//     let mut s1:String = String::from("Hello");
+//     append_string(&s1);
+//     println!("the new string is {}", s1);
+// }
+// fn append_string(s3:&String) {
+//     s3.push_str("world");
+// }
+
+// above commented code will throw error because we can not make change when we are boorwing \
+// for solution of this we will use mutable reference . 
