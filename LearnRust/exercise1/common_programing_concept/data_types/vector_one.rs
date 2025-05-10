0de4a8009eac984008578af16fe7c7571db84b74
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-11 00:40:37 +0530

    Basic vector declaration and syntax

---
+++ LearnRust/exercise1/common_programing_concept/data_types/vector_one.rs
@@ -0,0 +1,11 @@
+// vactor - Dynamic Array
+ fn main(){
+    let mut v:Vec<i32> = Vec::new(); //declaration
+    //let mut v = Vec::<i32>::new(); //another way of declaration
+    v.push(1);
+    v.push(2);
+    v.push(3);
+   //let mut v = vec![1,2,3,4,5];
+
+    println!("Vector v={:?}",v);
+ }
