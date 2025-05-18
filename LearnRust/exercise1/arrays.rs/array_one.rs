c843328d486de5434f387af88414ed76da104856
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-18 23:54:05 +0530

    Static and Dynamic  array Basic Program

--- LearnRust/exercise1/arrays.rs/array_one.rs
+++ LearnRust/exercise1/arrays.rs/array_one.rs
@@ -4,4 +4,8 @@
 fn main(){
     let number :[i32;3]= [1,2,3];
     println!("the static array is {:?}",number);
+    let mut dynamic_array= Vec::new();
+    dynamic_array.push(1);
+    dynamic_array.push(2);
+    println!("the dynamic array is{:?}",dynamic_array);
 }
