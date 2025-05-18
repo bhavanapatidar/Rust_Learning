94e4f2d16299a9f309155862b42c30c6ffb200a4
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-18 23:50:19 +0530

    Static array

---
+++ LearnRust/exercise1/arrays.rs/array_one.rs
@@ -0,0 +1,7 @@
+// Static Array: Make an array of 3 numbers: [1, 2, 3]. You can’t add a 4th number.
+// Dynamic Array: Start with an empty list, add 1, 2, 3, then add 4. Output: [1, 2, 3, 4].
+
+fn main(){
+    let number :[i32;3]= [1,2,3];
+    println!("the static array is {:?}",number);
+}
