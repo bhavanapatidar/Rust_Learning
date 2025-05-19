e8233bfe2d5c4c3778358e28fbafeff8ba5de8dc
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-05-19 22:57:33 +0530

    Mutable Structure in Rust

---
+++ LearnRust/exercise1/structs_to_structure/struct_two.rs
@@ -0,0 +1,24 @@
+// mutable reference allows you to change the value of the borrowed data.
+
+// struct like as class in other languages
+struct user{
+    name: String,
+    company: String,
+    age: u32,
+}
+
+fn main(){
+    let mut user1 =  user{  // creating instance of struct and assigning values , we can also assign in any order
+        name: String::from("Ali"),
+        company: String::from("Turing"),
+        age: 25,
+    };
+    let user2 = user{  // creating instance of struct and assigning values , we can also assign in any order
+        name: String::from("Ali"),
+        company: String::from("Turing"),
+        age: 25,
+    };
+    user1.name = String::from("Ali Raza");
+    println!("the name of user is {} and the company is {} and the age is {}",user1.name,user1.company,user1.age);
+    println!("the name of user is {} and the company is {} and the age is {}",user2.name,user2.company,user2.age);
+}
